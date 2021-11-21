d0997edc9a7e374dac4cc3138b5c2af78c7eec34
code-warren <warrenphilibert@gmail.com>
2021-11-21 20:06:08 -0300

    mouse events 1h10

--- cours/index.js
+++ cours/index.js
@@ -1,4 +1,33 @@
 //SELECTEURS
 // document.querySelector("h4").style.background = "yellow";
+// const baliseHTML = document.querySelector("h4");
 
-const
+// Click event
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+questionContainer.addEventListener("click", () => {
+  questionContainer.classList.toggle("question-clicked");
+});
+
+btn1.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "green";
+});
+
+btn2.addEventListener("click", () => {
+  response.style.visibility = "visible";
+  response.style.background = "red";
+});
+
+/*<div> > #id .class > baliseHTML*/
+
+//--------------------------------------------------------------------
+//Mouse Events
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+  console.log(e.target);
+});
