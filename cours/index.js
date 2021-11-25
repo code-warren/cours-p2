c9845bb3cbbfbd0bb95463a9dad441c956da4913
code-warren <warrenphilibert@gmail.com>
2021-11-25 15:09:31 -0300

    mouse envents 1h26

--- cours/index.js
+++ cours/index.js
@@ -29,5 +29,18 @@ btn2.addEventListener("click", () => {
 const mousemove = document.querySelector(".mousemove");
 
 window.addEventListener("mousemove", (e) => {
-  console.log(e.target);
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+});
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  mousemove.style.border = "2px solid teal";
+});
+questionContainer.addEventListener("mouseenter", () => {
+  questionContainer.style.background = "rgba(0,0,0,0.6)";
 });
