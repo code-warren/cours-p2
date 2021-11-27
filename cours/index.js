5da7413c5e4fe847b7be3c894c4f8860ba50f20b
code-warren <warrenphilibert@gmail.com>
2021-11-27 20:52:24 -0300

    Générateur de bulles 3h53 début

--- cours/index.js
+++ cours/index.js
@@ -76,8 +76,7 @@ document.addEventListener("keypress", (e) => {
   } else {
     keypressContainer.style.background = "red";
   }
-
-  ring(e.key);
+  if (e.key === "z") ring(e.key);
 });
 
 //-------------------------------------------------------------
@@ -86,8 +85,6 @@ document.addEventListener("keypress", (e) => {
 const nav = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-
   if (window.scrollY > 120) {
     nav.style.top = 0;
   } else {
@@ -97,3 +94,167 @@ window.addEventListener("scroll", () => {
 
 //-------------------------------------------------------------
 // Form event
+const inputName = document.querySelector('input[type="text"]');
+const select = document.querySelector("select");
+const form = document.querySelector("form");
+let pseudo = "";
+let language = "";
+
+inputName.addEventListener("input", (e) => {
+  pseudo = e.target.value;
+});
+
+select.addEventListener("input", (e) => {
+  language = e.target.value;
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (cgv.checked) {
+    document.querySelector("form > div").innerHTML = `
+    <h3>Pseudo : ${pseudo}</h3>
+    <h4>Language préféré : ${language}</h4>
+    `;
+  } else {
+    alert("Veuillez accepter les CGV");
+  }
+});
+
+//-------------------------------------------------------------
+//Load event
+window.addEventListener("load", () => {
+  // console.log("document chargé !");
+});
+
+//-------------------------------------------------------------
+//ForEach
+// const boxes = document.getElementsByName(".box");
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+  box.addEventListener("click", (e) => {
+    e.target.style.transform = "scale(0.7)";
+  });
+});
+
+//-------------------------------------------------------------
+//addEventListener vs onclick
+// document.body.onclick = () => {
+//   console.log("click !");
+// };
+
+//Bubbling=> fin(de basel'eventlistener est paramétré en mode bubbling)
+document.body.addEventListener("click", () => {
+  console.log("click 1 !");
+});
+
+//Usecapture
+document.body.addEventListener(
+  "click",
+  () => {
+    console.log("click 2 !");
+  },
+  true
+);
+
+//aide:gomakethings
+
+//-------------------------------------------------------------
+//stop proppagation
+// questionContainer.addEventListener("click", (e) => {
+//   alert("test !");
+//   e.stopPropagation();
+// });
+
+//removeEventListener
+
+//-------------------------------------------------------------
+// BOM
+
+// console.log(window.innerHeight);
+//  console.log(window.scrollY);
+// window.open("http://google.com", "cours js", "height=600, width=800");
+//window.close()
+
+//Evénements adossés à window
+//alert("hello");
+
+//confirm
+btn2.addEventListener("click", () => {
+  confirm("Voulez vous vraiment vous tromper ?");
+});
+
+//prompt
+btn1.addEventListener("click", () => {
+  let answer = prompt("entrez votre nom !");
+
+  questionContainer.innerHTML += "<h3>Bravo" + answer + "</h3>";
+});
+
+// Timer, compte à rebours
+// setTimeout(() => {
+//   questionContainer.style.borderRaduis = "300px";
+// }, 2000);
+
+// let interval = setInterval(() => {
+//   document.body.innerHTML += `
+//      <div class='box'>
+//        <h2>Nouvelle Boite !</h2>
+//      </div>
+//   `;
+// }, 1000);
+
+// document.body.addEventListener("click", (e) => {
+//   e.target.remove();
+//   clearInterval(interval);
+// });
+
+//location
+// console.log(location.href);
+// console.log(location.host);
+// console.log(location.pathname);
+// console.log(location.search);
+// location.replace("http://twitter.fr");
+
+// window.onload = () => {
+//   location.href = "http://twitter.fr";
+// };
+
+//navigator
+// console.log(navigator.userAgent);
+
+// https://developer.mozilla.org/fr/docs/Web/API/Geolocation/getCurrentPosition
+
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0,
+// };
+
+// function success(pos) {
+//   var crd = pos.coords;
+
+//   console.log("Votre position actuelle est :");
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude : ${crd.longitude}`);
+//   console.log(`La précision est de ${crd.accuracy} mètres.`);
+// }
+
+// function error(err) {
+//   console.warn(`ERREUR (${err.code}): ${err.message}`);
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error, options);
+
+//history
+// console.log(history);
+// window.history.back();
+//history.go(-2)
+
+//-------------------------------------------------------------
+// SetProperty
+window.addEventListener("mousemove", (e) => {
+  nav.style.setProperty("--x", e.layerX + "px");
+  nav.style.setProperty("--y", e.layerY + "px");
+});
